33e8fce4db3c1795d6ad86c02622e202aec40361
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-03-12 14:31:46 +0530

    Address book main branch is being created

---
+++ main.py
@@ -0,0 +1,9 @@
+class AddressBookMain:
+
+    @staticmethod
+    def main():
+        print("Welcome to the Address Book Application!")
+        # Here you can add code to initialize the address book and handle user interactions
+
+    if __name__ == "__main__":
+        main()
