fe6cb4d8920c274c223c754a17423904909b274e
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-03-13 11:18:35 +0530

    Add . Indentation error is fixed

--- addressbookManager.py
+++ addressbookManager.py
@@ -1,19 +1,18 @@
 from addressbook import AddressBook
 class AddressBookManager:
    
-   def __init__(self):
-      self.books={}
+    def __init__(self):
+        self.books={}
 
-   def add_book(self,book_name):
+    def add_book(self,book_name):
         if book_name not in self.books:
             self.books[book_name]=AddressBook()
         else:   
             raise ValueError("Book already exists")
-    
-   def get_book(self,book_name):
+
+    def get_book(self,book_name):
         if book_name in self.books:
             return self.books[book_name]
         else:
             raise ValueError("Book not found")
-    
-        
+
