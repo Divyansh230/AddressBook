1899628039bae11320152fbc545fe5c3f4c783be
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-03-15 20:40:34 +0530

    Add . reading writing from csv file is being done

--- file_io.py
+++ file_io.py
@@ -1,3 +1,5 @@
+import csv
+
 class FileIO:
 
     @staticmethod
@@ -22,4 +24,48 @@ class FileIO:
 
         return contact
     
+
+    ## writing to csv file 
+    @staticmethod
+    def write_to_csv(contacts,filename='contacts.txt'):
+        
+        with open(filename,'w') as file:
+            writer=csv.writer(file)
+
+            writer.writerow(
+                ["First Name","Last Name","Address","City","State","Zip","Phone","Email"]
+            )
+
+            for contact in contacts.values():
+                  writer.writerow([
+                    contact.first_name,
+                    contact.last_name,
+                    contact.address,
+                    contact.city,
+                    contact.state,
+                    contact.zip_code,
+                    contact.phone_number,
+                    contact.email
+                ])
+                  
+        print('Contacts written to csv file successfully')
+
+    @staticmethod
+    def read_from_csv(filename="contacts.csv"):
+
+        contacts = []
+
+        with open(filename, "r") as file:
+
+            reader = csv.reader(file)
+
+            next(reader)
+
+            for row in reader:
+                contacts.append(row)
+
+        return contacts
+
     
+    
+
